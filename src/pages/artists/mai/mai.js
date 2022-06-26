2f9141171afbcb099f7e23b00c2db0a7db8becc7
barigyasi <metaphysicalartinstitute.com>
2022-06-26 18:36:57 -0500

    sizes

--- src/pages/artists/mai/mai.js
+++ src/pages/artists/mai/mai.js
@@ -4,13 +4,13 @@ import maibio from "./MAI-BIO2.svg"
 
 const Mai = () => {
   return (
-    <div id='parent'>
+    <div>
 
-    <div class="child" className='art'>
+    
     <img src={maibio}></img>
-    </div>
+    
      
-     <div class="child" className="thirdwebcard">
+    
       
       
       <iframe src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=0&rpcUrl=https%3A%2F%2Fspeedy-nodes-nyc.moralis.io%2F1eb60c18fc38af1c4ec7a096%2Feth%2Fmainnet" 
@@ -25,7 +25,7 @@ const Mai = () => {
 
     </div>
 
-    </div>
+   
   )
 }
 
