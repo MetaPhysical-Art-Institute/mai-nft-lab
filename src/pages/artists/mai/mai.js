47527e2b1783795f07c219484d2b2ee6770bccc8
barigyasi <metaphysicalartinstitute.com>
2022-06-30 18:34:10 -0500

    size

--- src/pages/artists/mai/mai.js
+++ src/pages/artists/mai/mai.js
@@ -13,7 +13,7 @@ const Mai = () => {
       
       
       <iframe title='membership token' src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=0&rpcUrl=https%3A%2F%2Fspeedy-nodes-nyc.moralis.io%2F1eb60c18fc38af1c4ec7a096%2Feth%2Fmainnet" 
-      width="400px"
+      width="300px"
        height="500px"
        frameborder="0">
        
