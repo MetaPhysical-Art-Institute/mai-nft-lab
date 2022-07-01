afba13c97f1acfa773ee193d36fa12c8bbfeeb6d
barigyasi <metaphysicalartinstitute.com>
2022-06-30 20:12:33 -0500

    home page change

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import welcome from "../assets/images/welcome.svg"
 import home1 from "../assets/images/home1.svg"
 import home2 from "../assets/images/home2.svg"
+import mobilestart from "../assets/images/mobilestart1.svg"
 import "./home.css"
 
 class Home extends Component {
@@ -18,6 +19,9 @@ class Home extends Component {
         <img src={welcome}></img>
         <img src={home1}></img>
         <img src={home2}></img>
+        <div className='mobile'>
+          <p>* Tap the "🧪 " at the top of the screen to get started *</p>
+        </div>
         </center>
       </div>
       </div>
