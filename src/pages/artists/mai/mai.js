99ac5ee4be3d51af56f4609db6e6ecb8f80114ae
barigyasi <metaphysicalartinstitute.com>
2022-07-16 01:15:19 -0500

    bodega fix

--- src/pages/artists/mai/mai.js
+++ src/pages/artists/mai/mai.js
@@ -12,7 +12,7 @@ const Mai = () => {
     
       
       
-      <iframe title='membership token' src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=0&rpcUrl=https%3A%2F%2Fspeedy-nodes-nyc.moralis.io%2F1eb60c18fc38af1c4ec7a096%2Feth%2Fmainnet" 
+      <iframe title='membership token' src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=0&rpcUrl=https://eth-mainnet.g.alchemy.com/v2/LYhDJbJbPS2CSU5OyMnjyKqe7X7F1Eg6" 
       width="300px"
        height="500px"
        frameborder="0">
