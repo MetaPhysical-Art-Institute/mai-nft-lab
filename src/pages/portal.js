b6660e7ea3bc7a34e0c181f2da7edf7130ed2dc4
barigyasi <barigyasi@gmail.com>
2022-08-07 10:09:21 -0500

    remove discord widget

--- src/pages/portal.js
+++ src/pages/portal.js
@@ -142,11 +142,7 @@ function Portal() {
             </div>
            </section>
              
-           <Widget>
-            <div className='discord'>
-           <iframe src="https://discord.com/widget?id=936507135561433128&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-            </div>
-           </Widget>
+         
 
            <Button>
              <a href="https://discord.gg/GgAYeUpVkW">Click here to join our Discord</a> 
