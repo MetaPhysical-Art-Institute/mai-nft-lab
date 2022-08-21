354324ed1fd930dd60509c9b0552e7be4d06c51c
barigyasi <barigyasi@gmail.com>
2022-08-21 13:55:41 -0500

    listing Id update

--- src/pages/artists/mai/mai.js
+++ src/pages/artists/mai/mai.js
@@ -12,7 +12,7 @@ const Mai = () => {
     
       
       
-      <iframe title='membership token' src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=0&rpcUrl=https://eth-mainnet.g.alchemy.com/v2/LYhDJbJbPS2CSU5OyMnjyKqe7X7F1Eg6" 
+      <iframe title='membership token' src="https://gateway.ipfscdn.io/ipfs/Qmb7uH1FozWt9zG15fYzmrBM9AvzvfQGU43hinmY4sEQTv/marketplace.html?contract=0xd50CeE77dff5A6506e7b95Cb3e4EeA1fD3128F49&chainId=1&listingId=3&rpcUrl=https://eth-mainnet.g.alchemy.com/v2/LYhDJbJbPS2CSU5OyMnjyKqe7X7F1Eg6" 
       width="300px"
        height="500px"
        frameborder="0">
